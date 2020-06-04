67b031865121a01118153c131fe7fb2f186fe4c6
 alex lipnik <alexlipnik@alexs-MacBook-Pro.local>
2020-06-04 20:50:29 +0300

    added chat funtionality

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const app = express();
+const socketio = require('socket.io');
+const http = require('http');
+
+const { addUser, removeUser, getUser, getUsersInRoom } = require('./users'); 
 
 const cookieParser = require('cookie-parser');
 const mongoose = require('mongoose');
@@ -14,6 +18,48 @@ mongoose.connect('mongodb://localhost:27017/mernauth', {useUnifiedTopology: true
 const userRouter = require('./routes/User');
 app.use('/user', userRouter)
 
-app.listen(5000, () => {
+const server = http.createServer(app);
+const io = socketio(server)
+
+app.get('/', (req, res) => {
+    res.send('Server running')
+})
+
+io.on('connection', (socket) => {
+    console.log('We have a new connection.');
+
+    socket.on('join', ({name, room}, callback) => {
+        const {error, user} = addUser({ id: socket.id, name, room});
+
+        if(error) return callback(error)
+    
+        socket.emit('message', {user: 'admin', text: `${user.name}, welcome to the room ${user.room}`});
+        socket.broadcast.to(user.room).emit('message', {user: 'admin', text: `${user.name}, has joined!`});
+
+        socket.join(user.room);
+        
+        io.to(user.room).emit('roomData', {room: user.room, users: getUsersInRoom(user.room)})
+        callback();
+    })
+
+    socket.on('sendMessage', (message, callback) => {
+        const user = getUser(socket.id);
+
+        io.to(user.room).emit('message', {user: user.name, text: message})
+        io.to(user.room).emit('roomData', {user: user.name, text: message})
+
+        callback();
+    });
+
+    socket.on('disconnect', () => {
+        const user = removeUser(socket.id);
+
+        if(user) {
+            io.to(user.room).emit('message', {user: 'admin', text: `${user.name} has left`})
+        }
+    })
+})
+
+server.listen(8000, () => {
     console.log('express server started')
 })
